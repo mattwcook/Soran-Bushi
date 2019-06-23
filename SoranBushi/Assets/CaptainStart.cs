ee716bb066543dac4c41fb7dc8afee829dd8cf9a
Evie Powell <evie@vergeofbrilliance.com>
2019-06-23 12:45:15 -0700

    last minute FMOD

---
+++ SoranBushi/Assets/CaptainStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptainStart : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string voiceActingEventString = "event:/";
+
+    public FMOD.Studio.EventInstance voiceActingEvent;
+
+    [SerializeField]
+    DancePlayback captain;
+
+    private void Update()
+    {
+        voiceActingEvent = FMODUnity.RuntimeManager.CreateInstance(voiceActingEventString);
+        voiceActingEvent.start();
+        //if(Input.GetKeyDown(KeyCode.S))
+        //{
+            captain.ChangeName("captain_0");
+            captain.StartPlayback();
+        //}
+
+    }
+}
---
+++ SoranBushi/Assets/UIFollow.cs
@@ -0,0 +1,28 @@
+﻿// Smooth towards the target
+
+using UnityEngine;
+using System.Collections;
+
+public class UIFollow : MonoBehaviour
+{
+    Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    PlayerScript player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerScript>();
+        //target = player.Head.transform;
+    }
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.transform.TransformPoint(new Vector3(0, 0, 3f));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.LookAt(target);
+    }
+}
