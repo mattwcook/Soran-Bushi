e5249aa8e5e87818b5c31a672f3c680a6986a521
mattwcook <32008216+mattwcook@users.noreply.github.com>
2019-06-22 17:43:42 -0700

    Tutorial Progress

--- SoranBushi/Assets/Scripts/TutorialManager.cs
+++ SoranBushi/Assets/Scripts/TutorialManager.cs
@@ -11,13 +11,19 @@ public class TutorialManager : MonoBehaviour
     float timer = 0.0f;
 
     AudioSource helloGibberish;
+    AudioSource followMeGibberish;
+    AudioSource wellDoneGibberish;
+    AudioSource letsStartGibberish;
     // Start is called before the first frame update
     void Start()
     {
         helloGibberish = shipCaptain.transform.Find("Hello Audio").gameObject.GetComponent<AudioSource>();
+        followMeGibberish = shipCaptain.transform.Find("Follow Me Audio").gameObject.GetComponent<AudioSource>();
+        wellDoneGibberish = shipCaptain.transform.Find("Well Done Audio").gameObject.GetComponent<AudioSource>();
+        letsStartGibberish = shipCaptain.transform.Find("Lets Start Audio").gameObject.GetComponent<AudioSource>();
 
         ////////// Captain start wave hello
-        //shipCaptain.ChangeName(file.Name.Replace());
+        //shipCaptain.GetComponent<DancePlayback>().ChangeName(file.Name.Replace());
 
         ////////// Captain start intro audio
         helloGibberish.Play();
@@ -27,7 +33,21 @@ public class TutorialManager : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+
+        ////// After enough waves, turn off welcome text, turn on follow shadow, inititite first 
+        ////// move at low speed, initiate follow me text, initiate follow me audio
+        /*
+        if (timer >= )
+        {
+            timer = 0
+        }
+        */
         
+    }
+
+    void PlaybackEnded()
+    {
+        playbackCounter += 1;
     }
     
 }
